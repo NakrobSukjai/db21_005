673f03e7d8d5cf837073043d2fe341b28cfb4ec8
ITTIPISO <trirattree1112@gmail.com>
2021-10-01 22:11:59 +0700

    Update index_billdetail.php

--- views/billdetail/index_billdetail.php
+++ views/billdetail/index_billdetail.php
@@ -2,6 +2,7 @@
 <table border="1">
 <tr><td>BillID</td>
 <td>ProductID</td>
+<td>ProductName</td>
 <td>ColorID</td>
 <td>Quantity</td>
 <td>PrintColors</td>
@@ -14,6 +15,7 @@ New BillDetail [<a href="?controller=billdetail&action=newbilldetail">click</a>]
         echo"
         <tr><td>$billdetail->billid</td>
         <td>$billdetail->productid</td>
+        <td>$billdetail->productname</td>
         <td>$billdetail->colorid</td>
         <td>$billdetail->quantity</td>
         <td>$billdetail->numcolor</td>
