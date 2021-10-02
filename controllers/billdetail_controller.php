56e531cb01a127492c61246b8672b2b93fd77ae0
ITTIPISO <trirattree1112@gmail.com>
2021-10-02 16:11:34 +0700

    Update billdetail_controller.php

--- controllers/billdetail_controller.php
+++ controllers/billdetail_controller.php
@@ -6,4 +6,11 @@ class BillDetailController
         $billdetailList=BillDetail::getAll();
         require_once('views/billdetail/index_billdetail.php');
     }
+    public function newBilldetail()
+    {
+        $productcolorList=ProductColors::getAll();
+        $productList=Product::getAll();
+        $colorList=Colors::getAll();
+        require_once("views/billdetail/newBilldetail.php");
+    }
 }
