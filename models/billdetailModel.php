fbd3024bae019d9bba2b93ed9672384360b7bae1
ITTIPISO <trirattree1112@gmail.com>
2021-10-02 22:00:13 +0700

    Update billdetailModel.php

--- models/billdetailModel.php
+++ models/billdetailModel.php
@@ -44,4 +44,27 @@ class BillDetail
         require("connection_close.php");
         return "add success $result rows";
     }
+    public static function search($key)
+    {
+        require("connection_connect.php");
+        $sql ="SELECT * FROM BillDetail
+        INNER JOIN Product ON Product.Product_ID=BillDetail.Product_ID
+        INNER JOIN Colors ON Colors.Color_ID=BillDetail.Color_ID
+        where(BillDetail.Bill_ID like '%$key%' or Product.Product_ID like '%$key%' or 
+        Colors.Color_ID like '%$key%')";
+        $result=$conn->query($sql);
+        while($my_row=$result->fetch_assoc())
+        {
+            $Bill_ID=$my_row[Bill_ID];
+            $Product_ID=$my_row[Product_ID];
+            $P_Name=$my_row[P_Name];
+            $Color=$my_row[Color_ID];
+            $colorname=$my_row[C_Name];
+            $Quantity=$my_row[Quantity];
+            $numcolor=$my_row[numcolor];
+            $billdetailList[]=new BillDetail($Bill_ID,$Product_ID,$P_Name,$Color,$colorname,$Quantity,$numcolor);
+        }
+        require("connection_close.php");
+        return $billdetailList;
+    }
 }
