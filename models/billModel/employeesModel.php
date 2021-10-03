66a094901b51460f8f55410e44567debe6d83a5f
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-10-04 01:44:44 +0700

    Create employeesModel.php

---
+++ models/billModel/employeesModel.php
@@ -0,0 +1,31 @@
+<?php
+class Employees
+{
+    public $emp_id , $emp_name ;
+    public function __construct($emp_id , $emp_name)
+    {
+        $this-> emp_id = $emp_id ;
+        $this-> emp_name = $emp_name ;
+    }
+    public static function getALL()
+    {
+        $empList = [] ;
+        require("connection_connect.php");
+        $sql = "SELECT * From Employees as emp
+        ORDER BY emp.EMP_ID"
+        $result = $conn -> query($sql);
+        while($my_row = $result -> fetch_assoc())
+        {
+            $EMP_ID = $my_row[EMP_ID];
+            $E_Name = $my_row[E_Name];
+
+            $empList[] = new Employees($EMP_ID , $E_Name);
+        }
+        require("connection_close.php");
+        return $empList ;
+    }
+    
+
+
+
+}
