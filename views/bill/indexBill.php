8a792d075922224ae1840aefcd759f1b6dec527e
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-10-04 02:26:46 +0700

    Create indexBill.php

---
+++ views/bill/indexBill.php
@@ -0,0 +1,25 @@
+==================== ภควัตร บุญศรี 6220504747======================<br>
+<table border="1">
+<tr><td>รหัสใบเสนอราคา</td>
+<td>วันที่</td>
+<td>เงื่อนไขการจ่าย</td>
+<td>พนักงาน</td>
+<td>ลูกค้า</td>
+<td>Update</td>
+<td>Delete</td>
+<?php
+    foreach($billList as $bill)
+    {
+        echo"
+        <tr><td>$bill->bill_id</td>
+        <td>$bill->date</td>
+        <td>$bill->P_Name</td>
+        <td>$bill->emp_name</td>
+        <td>$bill->cus_name</td>
+        <td>$bill->cus_name</td>
+        <td>update</td>
+        <td>delete</td>
+        ";
+    }
+    echo "</table>";
+?>
