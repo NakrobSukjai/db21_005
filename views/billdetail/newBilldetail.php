5296cf8d0169d05f4fe9e6399c9fbd046b6fe01f
ITTIPISO <trirattree1112@gmail.com>
2021-10-03 20:34:23 +0700

    Update newBilldetail.php

--- views/billdetail/newBilldetail.php
+++ views/billdetail/newBilldetail.php
@@ -1,7 +1,7 @@
 <form method="get" action="">
     <label>รหัสใบสั่งซื้อ <select name="Bill_ID">
-        <?php foreach($billdetailList as $billdetail)
-        {echo "<option value=$billdetail->billid>$billdetail->billid</option>";}?>
+        <?php foreach($billList as $bill)
+        {echo "<option value=$bill->billid>$bill->billid</option>";}?>
         </select></label><br>
     <table border="1">
     รายชื่อและสีของสินค้า<br>
@@ -15,7 +15,7 @@
             echo "</table>";
         ?>
     <label>สินค้า <select name="Product_ID">
-    <?php foreach($productcolorList as $pro){
+    <?php foreach($productList as $pro){
         echo "<option value = $pro->productid>
         $pro->productname</option>";}?>
     </select></label><br>
@@ -23,7 +23,7 @@
     <label>สีของสินค้า <select name="Color_ID">
         <?php foreach($colorList as $color)
         {echo "<option value=$color->colorid>$color->colorname</option>";}?>
-        </select></label><br>
+    </select></label><br>
     <label>จำนวนสินค้า <input type="number" name="Quantity"/></label><br>
     <label>จำนวนสีที่ต้องการ <input type="number" name="numcolor"/></label><br>
     <input type="hidden" name="controller" value="billdetail"/>
