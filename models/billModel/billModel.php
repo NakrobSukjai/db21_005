a54f55dbe873f8dedbf2edce4918b8c2aadeffdf
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-10-04 01:44:51 +0700

    Update billModel.php

--- models/billModel/billModel.php
+++ models/billModel/billModel.php
@@ -1,29 +1,37 @@
 <?php
 class Bill
 {
-    public $bill_id , $date , $paymentCondi , $emp_id , $cus_id 
+    public $bill_id , $date , $P_Name , $emp_name , $cus_name 
 
-    public function __construct($bill_id, $date , $paymentCondi , $emp_id , $cus_id)
+    public function __construct($bill_id, $date , $P_Name , $emp_name , $cus_name)
     {
         $this->bill_id = $bill_id;
         $this->date = $date
-        $this->paymentCondi = $paymentCondi
-        $this->emp_id = $emp_id
-        $this->cus_id = $cus_id
+        $this->P_Name = $P_Name
+        $this->emp_name = $emp_name
+        $this->cus_name = $cus_name
     }
 
     public static function getAll()
     {
         $billList = [] ;
         require("connection_connect.php");
-        $sql = "SELECT * from Bill ORDER BY Bill.Bill_ID";
+        $sql = "SELECT * from Bill as b
+        INNER JOIN Employees as emp ON emp.EMP_ID = b.EMP_ID
+        INNER JOIN Customer as cus ON cus.Customer_ID = b.Customer_ID
+        ORDER BY b.Bill_ID ASC";
         $result = $conn -> query($sql);
         while($my_row = $result->fetch_assoc())
         {
             $Bill_ID =  $my_row[Bill_ID];
-            $Date = $my_row
-
+            $Date = $my_row[Date];
+            $Pay_Name = $my_row[Pay_Name];
+            $E_Name = $my_row[E_Name];
+            $C_Name = $my_row[C_Name]
+            $billList[] = new Bill($Bill_ID,$Date,$Pay_Name,$E_Name,$C_Name);
         }
+        require("connection_close.php")
+        return $billList ;
 
     }
 
