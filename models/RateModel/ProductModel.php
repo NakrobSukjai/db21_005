a8278296c5a636f8cec73c64a92b86586df467c7
MIIKI <nakrob.s@ku.th>
2021-10-04 05:00:18 +0700

    Update ProductModel.php

--- models/RateModel/ProductModel.php
+++ models/RateModel/ProductModel.php
@@ -1,9 +1,9 @@
 <?php
 class Product{
     public $productid,$productname;
-    public function __construct($P_ID,$P_Name)
+    public function __construct($Product_ID,$P_Name)
     {
-        $this->productid=$P_ID;
+        $this->productid=$Product_ID;
         $this->productname=$P_Name;
     }
     public static function getAll()
@@ -14,9 +14,9 @@ class Product{
         $result =$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
-            $P_ID=$my_row[Product_ID];
+            $Product_ID=$my_row[Product_ID];
             $P_Name=$my_row[P_Name];
-            $productList[]=new Product($P_ID,$P_Name);
+            $productList[]=new Product($Product_ID,$P_Name);
         }
         require("connection_close.php");
         return $productList;
