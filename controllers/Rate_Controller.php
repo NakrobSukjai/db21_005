195f2c034fe7ec7124f1e28a739e7222acfcbb9c
MIIKI <nakrob.s@ku.th>
2021-10-04 03:12:56 +0700

    Update Rate_Controller.php

---
+++ controllers/Rate_Controller.php
@@ -0,0 +1,75 @@
+<?php 
+class RateController
+{
+    public function index()
+    {
+        $RateList=Rate::getAll();
+        require_once('views/Rate/index_Rate.php');
+    }
+    public function newRate()
+    {
+        $productList=Product::getAll();
+        require_once("views/Rate/newRate.php");
+    }
+    public function addRate()
+    {
+        $productid=$_GET['Product_ID'];
+        $rateid=$_GET['Rate_ID'];
+        $quantity=$_GET['Quantity'];
+        $price=$_GET['Price'];
+        $priceaddpercolor=$_GET['PriceAddPerColor'];
+        Rate::Add($productid,$rateid,$quantity,$price,$priceaddpercolor);
+        RateController::index();
+    }
+    public function searchRate(){
+        $key=$_GET['key'];
+        $RateList=Rate::search($key);
+        require_once('views/Rate/index_Rate.php');
+    }
+    public function updateFormRate()
+    {
+        $productid=$_GET['Product_ID'];
+        $rateid=$_GET['Rate_ID'];
+        $quantity=$_GET['Quantity'];
+        $price=$_GET['Price'];
+        $priceaddpercolor=$_GET['PriceAddPerColor'];
+        $productList=Product::getAll();
+        $Rate=Rate::get($productid,$rateid,$quantity,$price,$priceaddpercolor);
+        require_once('views/Rate/updateFormRate.php');
+    }
+    public function updateRate()
+    {
+        $productid=$_GET['Product_ID'];
+        $rateid=$_GET['Rate_ID'];
+        $quantity=$_GET['Quantity'];
+        $price=$_GET['Price'];
+        $priceaddpercolor=$_GET['lastPriceAddPerColor'];
+        $lastproductid=$_GET['lastProduct_ID'];
+        $lastrateid=$_GET['lastRate_ID'];
+        $lastquantity=$_GET['lastQuantity'];
+        $lastprice=$_GET['lastPrice'];
+        $lastpriceaddpercolor=$_GET['lastPriceAddPerColor'];
+        Rate::update($productid,$rateid,$quantity,$price,$priceaddpercolor,$lastproductid,$lastrateid,$lastquantity,$lastprice,$lastpriceaddpercolor);
+        RateController::index();
+    }
+    public function deleteConfirmRate()
+    {
+        $productid=$_GET['Product_ID'];
+        $rateid=$_GET['Rate_ID'];
+        $quantity=$_GET['Quantity'];
+        $price=$_GET['Price'];
+        $priceaddpercolor=$_GET['lastPriceAddPerColor'];
+        $Rate=Rate::get($productid,$rateid,$quantity,$price,$priceaddpercolor);
+        require_once('views/Rate/deleteConfirmRate.php');
+    }
+    public function deleteRate()
+    {
+        $productid=$_GET['Product_ID'];
+        $rateid=$_GET['Rate_ID'];
+        $quantity=$_GET['Quantity'];
+        $price=$_GET['Price'];
+        $priceaddpercolor=$_GET['lastPriceAddPerColor'];
+        Rate::delete($productid,$rateid,$quantity,$price,$priceaddpercolor);
+        RateController::index();
+    }
+}
