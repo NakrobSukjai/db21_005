0585dfba37e178ba44f03aff114fce4714727c51
ITTIPISO <trirattree1112@gmail.com>
2021-10-03 18:22:23 +0700

    Update updateFormBilldetail.php

--- views/billdetail/updateFormBilldetail.php
+++ views/billdetail/updateFormBilldetail.php
@@ -2,19 +2,18 @@
     <label>รหัสใบสั่งซื้อ <select name="Bill_ID">
         <?php foreach($quotation_list as $quotation){
           echo "<option value = $quotation->Q_ID";
-          if($quotation->Q_ID==$billdetail->Qid){echo " selected='selected'";}
+          if($quotation->Q_ID=$billdetail->Qid){echo " selected='selected'";}
           echo ">$quotation->Q_ID</option>";
         }?>
     </select></label><br>
 
     <label>สินค้าและสีที่ต้องการ <select name="Product_ID">
         <?php foreach($productcolorList as $pro){
-        echo "<option value = $pro->procID";
-        if($pro->productid==$billdetail->productid){echo " selected='selected'";}
+        echo "<option value = $pro->productid";
+        if($pro->productid=$billdetail->productid){echo " selected='selected'";}
         echo ">$pro->productname $pro->colorname</option>";
         }?>
     </select></label><br>
-
     <label>จำนวนสินค้า <input type="number" name="Quantity"/></label><br>
     <label>จำนวนสีที่ต้องการ <input type="number" name="numcolor"/></label><br>
     <input type="hidden" name="controller" value="billdetail"/>
