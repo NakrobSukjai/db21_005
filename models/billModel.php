7326e7c17c4ca69d2cef04280ade35ae57d545c6
ITTIPISO <trirattree1112@gmail.com>
2021-10-03 20:34:18 +0700

    Create billModel.php

---
+++ models/billModel.php
@@ -0,0 +1,25 @@
+<?php 
+class Bill
+{
+    public $billid;
+    public function __construct($Bill_ID)
+    {
+        $this->billid=$Bill_ID;
+    }
+    public static function getAll()
+    {
+        $billList=[];
+        require("connection_connect.php");
+        $sql ="SELECT * FROM Bill
+        ORDER BY Bill.Bill_ID";
+        $result=$conn->query($sql);
+        while($my_row=$result->fetch_assoc())
+        {
+            $Bill_ID=$my_row[Bill_ID];
+            $billList[]=new Bill($Bill_ID);
+        }
+        require("connection_close.php");
+        return $billList;
+    }
+}
+?>
