da5546a5b729e956437f4d4f1200670353a7a42b
MIIKI <nakrob.s@ku.th>
2021-10-04 17:12:00 +0700

    Update updateFormRate.php

--- views/rate/updateFormRate.php
+++ views/rate/updateFormRate.php
@@ -13,11 +13,11 @@
     <label>PriceAddPerColor <input type="number" name="PriceAddPerColor"
         value="<?php echo $rate->priceaddpercolor;?>"/></label><br>
     <input type="hidden" name="controller" value="rate"/>
-    <input type="hidden" name="lastP_Name" value="<?php echo $rate->P_Name;?>"/>
-    <input type="hidden" name="lastPPP_ID" value="<?php echo $rate->rateid;?>"/>
-    <input type="hidden" name="lastQuantity" value="<?php echo $rate->quantity;?>"/>
-    <input type="hidden" name="lastPrice" value="<?php echo $rate->price;?>"/>
-    <input type="hidden" name="lastPriceAddPerColor" value="<?php echo $rate->priceaddpercolor;?>"/>
+    <input type="hidden" name="lastP_Name" value="<?php echo $rate->lastP_Name;?>"/>
+    <input type="hidden" name="lastPPP_ID" value="<?php echo $rate->lastrateid;?>"/>
+    <input type="hidden" name="lastQuantity" value="<?php echo $rate->lastquantity;?>"/>
+    <input type="hidden" name="lastPrice" value="<?php echo $rate->lastprice;?>"/>
+    <input type="hidden" name="lastPriceAddPerColor" value="<?php echo $rate->lastpriceaddpercolor;?>"/>
     <button type="submit" name="action" value="updateRate">update</button>
     <button type="submit" name="action" value="index">Back</button>
     </form>
