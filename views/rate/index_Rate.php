9c8fc58880b37718426b083de1578ed0e5a9c4a9
MIIKI <nakrob.s@ku.th>
2021-10-04 17:03:16 +0700

    Update index_Rate.php

--- views/rate/index_Rate.php
+++ views/rate/index_Rate.php
@@ -8,11 +8,11 @@
 <td>PriceAddPerColor</td>
 <td>Update</td>
 <td>Delete</td>
-new rate [<a href="?controller=rate&action=newRate">click</a>]<br>
+New Rate [<a href="?controller=rate&action=newrate">click</a>]<br>
 <form method="get" action="">
     <input type="text" name="key">
     <input type="hidden" name="controller" value="rate"/>
-    <button type="submit" name="action" value="searchRate">Search</button>
+    <button type="submit" name="action" value="searchrate">Search</button>
 </form>
 <?php
     foreach($rateList as $rate)
@@ -25,7 +25,7 @@ new rate [<a href="?controller=rate&action=newRate">click</a>]<br>
         <td>$rate->price</td>
         <td>$rate->priceaddpercolor</td>
 
-        <td>update</td>
+        <td><a href=?controller=rate&action=updateFormrate&P_Name=$rate->P_Name&PPP_ID=$rate->rateid&Quantity=$rate->quantity&Price=$rate->price&PriceAddPerColor=$rate->priceaddpercolor>update</td>
         <td>delete</td>
         ";
     }
