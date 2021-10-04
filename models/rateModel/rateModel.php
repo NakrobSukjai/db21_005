e19ecce4ff947e6e6c12b5e55a1f2b0e55672920
ITTIPISO <trirattree1112@gmail.com>
2021-10-04 18:49:52 +0700

    Update rateModel.php

--- models/rateModel/rateModel.php
+++ models/rateModel/rateModel.php
@@ -92,8 +92,8 @@ class Rate{
     public static function update($P_Name,$quantity,$price,$priceaddpercolor,$lastP_Name,$lastquantity,$lastprice,$lastpriceaddpercolor)
     {
         require("connection_connect.php");
-        $sql="UPDATE PPPOeders SET Product.Product_ID='$P_Name',PPPOeders.PPP_ID='$rateid',PPPOeders.Quantity='$quantity',PPPOeders.Price=$price,PPPOeders.PriceAddPerColor=$priceaddpercolor
-        WHERE  Product.Product_ID='$lastP_Name' AND PPPOeders.Quantity='$lastquantity' AND PPPOeders.Price=$lastprice AND PriceAddPerColor=$lastpriceaddpercolor";
+        $sql="UPDATE PPPOrders SET PPPOrders.Product_ID='$P_Name',PPPOrders.Quantity=$quantity ,PPPOrders.Price=$price,PPPOrders.PriceAddPerColor=$priceaddpercolor
+        WHERE  PPPOrders.Product_ID='$lastP_Name'AND PPPOrders.Quantity=$lastquantity AND PPPOrders.Price=$lastprice AND PPPOrders.PriceAddPerColor=$lastprice";
         $result=$conn->query($sql);
         require("connection_close.php");
         return "update success $result row";
