2c89abcb168247b3f0d7ff13ddf8e81c46d62f0d
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-10-04 21:55:12 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
     $controllers =array('pages'=>['home','error'],
                         'billdetail'=>['index','newBilldetail','addBilldetail','searchBilldetail','updateFormBilldetail','updateBilldetail','deleteConfirmBilldetail','deleteBilldetail']
-                        ,'bill' =>['index','newBill','addBill','search','updateFormBill']
+                        ,'bill' =>['index','newBill','addBill','search','updateFormBill','updateBill']
                         ,'rate' =>['index','newRate','addRate','searchRate','updateFormRate','updateRate','deleteConfirmRate','deleteRate']);
 
     function call($controller,$action)
