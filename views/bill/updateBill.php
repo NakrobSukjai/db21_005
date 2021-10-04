640d593e5da2e2314ef817291dbefb38f73f69f4
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-10-04 21:55:15 +0700

    Update updateBill.php

--- views/bill/updateBill.php
+++ views/bill/updateBill.php
@@ -33,6 +33,7 @@
         ?>
     </select></label><br>
     <input type="hidden" name = "controller" value ='bill'/>
-
+    <button type = "submit" name="action" value="index">Back</button>
+    <button type = "submit" name="action" value="updateBill">update</button>
 
 </form>
