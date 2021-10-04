48ea6e6afce198b17f4d344561dd9eb57abdfca1
MIIKI <nakrob.s@ku.th>
2021-10-04 18:23:06 +0700

    Update updateFormRate.php

--- views/rate/updateFormRate.php
+++ views/rate/updateFormRate.php
@@ -1,14 +1,13 @@
 <form method="get" action="">
-    <label>ProductName <select name="P_Name">
+    <label>ProductName <select name="Product_ID">
         <?php foreach($productList as $pro){
-        echo "<option value = $pro->P_Name";
-        if($rate->P_Name==$billdetail->P_Name){echo " selected='selected'";}
-        echo ">$pro->P_Name</option>";
+        echo "<option value = $pro->productid";
+        if($rate->productid==$rate->productid){echo " selected='selected'";}
+        echo ">$pro->productid</option>";
         }?>
     </select></label><br>
 
-    <label>Rate_ID <input type="number" name="PPP_ID"
-        value="<?php echo $rate->rateid;?>"/></label><br>
+
     <label>Quantity <input type="number" name="Quantity"
         value="<?php echo $rate->quantity;?>"/></label><br>
     <label>RatePrice <input type="number" name="Price"
@@ -16,8 +15,7 @@
     <label>PriceAddPerColor <input type="number" name="PriceAddPerColor"
         value="<?php echo $rate->priceaddpercolor;?>"/></label><br>
     <input type="hidden" name="controller" value="rate"/>
-    <input type="hidden" name="lastP_Name" value="<?php echo $rate->P_Name;?>"/>
-    <input type="hidden" name="lastPPP_ID" value="<?php echo $rate->rateid;?>"/>
+    <input type="hidden" name="lastProduct_ID" value="<?php echo $rate->productid;?>"/>
     <input type="hidden" name="lastQuantity" value="<?php echo $rate->quantity;?>"/>
     <input type="hidden" name="lastPrice" value="<?php echo $rate->price;?>"/>
     <input type="hidden" name="lastPriceAddPerColor" value="<?php echo $rate->priceaddpercolor;?>"/>
