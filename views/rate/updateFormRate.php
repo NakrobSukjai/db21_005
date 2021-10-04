1d6040503b539cc2da5404575721cd41b3c1bd54
MIIKI <nakrob.s@ku.th>
2021-10-04 18:32:51 +0700

    Update updateFormRate.php

--- views/rate/updateFormRate.php
+++ views/rate/updateFormRate.php
@@ -1,9 +1,9 @@
 <form method="get" action="">
     <label>ProductName <select name="P_Name">
         <?php foreach($productList as $pro){
-        echo "<option value = $pro->productid";
-        if($rate->productid==$rate->productid){echo " selected='selected'";}
-        echo ">$pro->productid</option>";
+        echo "<option value = $pro->P_Name";
+        if($rate->P_Name==$rate->P_Name){echo " selected='selected'";}
+        echo ">$pro->P_Name</option>";
         }?>
     </select></label><br>
 
@@ -15,7 +15,7 @@
     <label>PriceAddPerColor <input type="number" name="PriceAddPerColor"
         value="<?php echo $rate->priceaddpercolor;?>"/></label><br>
     <input type="hidden" name="controller" value="rate"/>
-    <input type="hidden" name="lastProduct_ID" value="<?php echo $rate->productid;?>"/>
+    <input type="hidden" name="lastProduct_ID" value="<?php echo $rate->P_Name;?>"/>
     <input type="hidden" name="lastQuantity" value="<?php echo $rate->quantity;?>"/>
     <input type="hidden" name="lastPrice" value="<?php echo $rate->price;?>"/>
     <input type="hidden" name="lastPriceAddPerColor" value="<?php echo $rate->priceaddpercolor;?>"/>
