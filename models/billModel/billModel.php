1315925c607ae70d7773d010c73ce99d10c54e72
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-10-04 18:04:17 +0700

    Update billModel.php

--- models/billModel/billModel.php
+++ models/billModel/billModel.php
@@ -47,6 +47,32 @@ class Bill
 
     }
 
+    public static function search($key)
+    {
+        require("connection_connect.php");
+        $sql = "SELECT * from Bill as b
+        INNER JOIN Employees as emp ON emp.EMP_ID = b.EMP_ID
+        INNER JOIN Customer as cus ON cus.Customer_ID = b.Customer_ID
+        INNER JOIN PaymentCondition as pay ON pay.Pay_ID = b.Pay_ID
+        WHERE (b.Bill_ID like '%$key%' or emp.E_Name like '%$key% or pay.Pay_Name like '%$key%'
+        or b.Date like '%$key%' or cus.C_Name like '%$key%')
+        ORDER BY b.Bill_ID ASC";
+        $result = $conn -> query($sql);
+        while($my_row = $result -> fetch_assoc())
+        {
+            $Bill_ID =  $my_row[Bill_ID];
+            $Date = $my_row[Date];
+            $Pay_Name = $my_row[Pay_Name];
+            $E_Name = $my_row[E_Name];
+            $C_Name = $my_row[C_Name];
+            
+            $billList[] = new Bill($Bill_ID,$Date,$Pay_Name,$E_Name,$C_Name);
+        }
+        require("connection_close.php");
+        return $billList;
+
+    }
+
 
 }
 ?>
