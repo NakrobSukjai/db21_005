c3e05e1094a1d2de01d98846829fe195705f09da
MIIKI <nakrob.s@ku.th>
2021-10-04 17:03:14 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
     $controllers =array('pages'=>['home','error'],
                         'billdetail'=>['index','newBilldetail','addBilldetail','searchBilldetail','updateFormBilldetail','updateBilldetail','deleteConfirmBilldetail','deleteBilldetail']
                         ,'bill' =>['index','newBill','addBill']
-                        ,'rate' =>['index','newRate','addRate','searchRate']);
+                        ,'rate' =>['index','newRate','addRate','searchRate','updateFormRate','updateRate']);
 
     function call($controller,$action)
     {
