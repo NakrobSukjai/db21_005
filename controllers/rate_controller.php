fcb3dff4282e5170cbd04a29f47ab155774dada1
MIIKI <nakrob.s@ku.th>
2021-10-04 15:40:34 +0700

    Update rate_controller.php

--- controllers/rate_controller.php
+++ controllers/rate_controller.php
@@ -5,4 +5,24 @@ class RateController{
         $rateList=Rate::getAll();
         require_once('views/rate/index_Rate.php');
     }
+        public function newRate()
+        {
+        $productList = Product::getAll();
+        require_once('views/rate/newRate.php');
+    }
+
+
+        public function addRate()
+    {
+        $P_Name = $_GET['P_Name'];
+        $rateid = $_GET['PPP_ID'];
+        $quantity = $_GET['Quantity'];
+        $price = $_GET['Price'];
+        $priceaddpercolor = $_GET['PriceAddPerColor'];
+
+        Rate::Add($P_Name,$rateid,$quantity,$price,$priceaddpercolor);
+        RateController::index();
+
+    }
 }
+?>
