62bf7932789e11b8dc81145627123cecbe29e196
MIIKI <nakrob.s@ku.th>
2021-10-04 15:43:03 +0700

    Update newRate.php

--- views/rate/newRate.php
+++ views/rate/newRate.php
@@ -1,5 +1,5 @@
 <form method="get" action="">
-    <label>P_Name <select name="Product_ID">
+    <label>P_Name <select name="P_Name">
     <?php foreach($productList as $pro){
         echo "<option value = $pro->productid>
         $pro->P_Name</option>";}?>
@@ -9,7 +9,7 @@
     <label>Quantity <input type="number" name="Quantity"/></label><br>
     <label>RatePrice <input type="number" name="Price"/></label><br>
     <label>PriceAddPerColor <input type="number" name="PriceAddPerColor"/></label><br>
-    <input type="hidden" name="controller" value="Rate"/>
+    <input type="hidden" name="controller" value="rate"/>
     <button type="submit" name="action" value="addRate">Save</button>
     <button type="submit" name="action" value="index">Back</button>
 </form>
